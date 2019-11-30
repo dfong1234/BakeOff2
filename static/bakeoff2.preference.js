fd07520b8640225699f3c83c787866a54b1bfedd
dfong1234 <56769286+dfong1234@users.noreply.github.com>
2019-11-29 22:46:38 -0800

    started update to preferences

--- static/bakeoff2.preference.js
+++ static/bakeoff2.preference.js
@@ -11,35 +11,22 @@ var color = 'dodgerblue';
 
 openTab(tabName, color) 
 
+//Initialize sliders
+$(".nutrient-slider").slider({
+	max: 100,
+	min: 0,
+	step: 1	
+});
 
+//Basal Metabolic Rate, Mifflin St Jeor Equation:
+function BMREquation(){
+	var s = $("#sex").val() == "Male" ? 5 : -161 ;
+	return ((10 * $("#weight").val()) + (6.25 * $("#height").val() * 100) - (5 * $("#age").val()) + s);
+};
 
+$("#dri-defaults").click(function() {
+	var calories = BMREquation();
+	$("#dri-calories").val(calories);
+	alert("Default calories restored.");
+});
 
-
-
-
-
-
-/*  ---  ---  */
-
-// --- Label Initialization ---
-
-
-// --- Variables ---
-
-
-
-// --- Subroutine Functions ---
-
-
-// --- In-Use ---
-
-
-/*  ---  ---  */
-
-
-// --- Variables ---
-
-// --- Subroutine Functions ---
-
-
-// --- In-Use ---
