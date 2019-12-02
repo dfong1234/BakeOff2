ce81efe93f711a5e7fced381ed09860931b89c0a
Riyya-HI <57051186+Riyya-HI@users.noreply.github.com>
2019-12-02 14:26:03 -0800

    Add files via upload

---
+++ DecisionTree1.js
@@ -0,0 +1,278 @@
+function dt(user_gender, user_age, user_carbs1, user_carbs2, user_fats1, user_fats2, user_prot1, user_prot2)
+{
+  var a[i] = {};
+  var i=0;
+  var b[i] = {};
+  var j=0;
+  if user_gender == "Male"
+  {
+  	if 0 < = user_age < 1
+  	{
+  		if user_carbs2 < 75
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Carbohydrates";
+  			i++;
+  		}
+
+  		if user_fats2 < 31
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Fats";
+  			i++;
+  		}
+  		if user_prot2 < 10
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Proteins";
+  			i++;
+  		}
+
+  	}
+  	else if 1 < = user_age < = 3
+  	{
+  		if user_carbs1 < 45 //&& user_carbs2 > 65
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Carbohydrates";
+  			i++;
+  		}
+  		if user_fats1 < 30 //&& user_carbs2 > 40
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Fats";
+  			i++;
+  		}
+  		if user_fats2 > 40 //&& user_carbs2 > 40
+  		{
+  			console.log("Bad choice");
+  			b[j] == "Fats";
+  			j++;
+  		}
+  		if user_prot1 < 5 //&& user_prot2 > 20
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Proteins";
+  			i++;
+        }
+  	}
+  	else if 4 < = user_age < = 8
+  	{
+  		if user_carbs1 < 45 //&& user_carbs2 > 65
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Carbohydrates";
+  			i++;
+  		}
+  		if user_fats1 < 25 //&& user_carbs2 > 35
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Fats";
+  			i++;
+  		}
+  		if user_fats2 > 35 //&& user_carbs2 > 40
+  		{
+  			console.log("Bad choice");
+  			b[j] == "Fats";
+  			j++;
+  		}
+  		if user_prot1 < 10 //&& user_prot2 > 30 
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Proteins";
+  			i++;
+  		}
+
+  	}
+  	else if 9 < = user_age < = 18
+  	{
+  		if user_carbs1 < 45 //&& user_carbs2 > 65
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Carbohydrates";
+  			i++;
+  		}
+  		if user_fats1 < 25 //&& user_carbs2 > 35
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Fats";
+  			i++;
+  		}
+  		if user_fats2 > 35 //&& user_carbs2 > 40
+  		{
+  			console.log("Bad choice");
+  			b[j] == "Fats";
+  			j++;
+  		}
+  		if user_prot1 < 10 //&& user_prot2 > 30 
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Proteins";
+  			i++;
+  		}
+
+  	}
+  	else 
+  	{
+  		if user_carbs1 < 45 //&& user_carbs2 > 65
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Carbohydrates";
+  			i++;
+  		}
+  		if user_fats1 < 20 //&& user_carbs2 > 35
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Fats";
+  			i++;
+  		}
+  		if user_prot1 < 10 //&& user_prot2 > 35 
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Proteins";
+  			i++;
+  		}
+
+  	}
+
+
+
+  }
+  else
+  {
+	if 0 < = user_age < 1
+  	{
+  		if user_carbs2 < 75
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Carbohydrates";
+  			i++;
+  		}
+
+  		if user_fats2 < 31
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Fats";
+  			i++;
+  		}
+  		if user_prot2 < 10
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Proteins";
+  			i++;
+  		}
+
+  	}
+  	else if 1 < = user_age < = 3
+  	{
+  		if user_carbs1 < 45 //&& user_carbs2 > 65
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Carbohydrates";
+  			i++;
+  		}
+  		if user_fats1 < 30 //&& user_carbs2 > 40
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Fats";
+  			i++;
+  		}
+  		if user_fats2 > 40 //&& user_carbs2 > 40
+  		{
+  			console.log("Bad choice");
+  			b[j] == "Fats";
+  			j++;
+  		}
+  		if user_prot1 < 5 .//&& user_prot2 > 20
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Proteins";
+  			i++;
+        }
+  	}
+  	else if 4 < = user_age < = 8
+  	{
+  		if user_carbs1 < 45 //&& user_carbs2 > 65
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Carbohydrates";
+  			i++;
+  		}
+  		if user_fats1 < 25 //&& user_carbs2 > 35
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Fats";
+  			i++;
+  		}
+  		if user_fats2 > 35 //&& user_carbs2 > 40
+  		{
+  			console.log("Bad choice");
+  			b[j] == "Fats";
+  			j++;
+  		}
+  		if user_prot1 < 10 //&& user_prot2 > 30 
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Proteins";
+  			i++;
+  		}
+
+  	}
+  	else if 9 < = user_age < = 18
+  	{
+  		if user_carbs1 < 45 //&& user_carbs2 > 65
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Carbohydrates";
+  			i++;
+  		}
+  		if user_fats1 < 25 //&& user_carbs2 > 35
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Fats";
+  			i++;
+  		}
+  		if user_fats2 > 35 //&& user_carbs2 > 40
+  		{
+  			console.log("Bad choice");
+  			b[j] == "Fats";
+  			j++;
+  		}
+  		if user_prot1 < 10 //&& user_prot2 > 30 
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Proteins";
+  			i++;
+  		}
+
+  	}
+  	else 
+  	{
+  		if user_carbs1 < 45 //&& user_carbs2 > 65
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Carbohydrates";
+  			i++;
+  		}
+  		if user_fats1 < 20 //&& user_carbs2 > 35
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Fats";
+  			i++;
+  		}
+  		if user_fats2 > 35 //&& user_carbs2 > 40
+  		{
+  			console.log("Bad choice");
+  			b[j] == "Fats";
+  			j++;
+  		}
+  		if user_prot1 < 10 //&& user_prot2 > 35 
+  		{
+  			console.log("Bad choice");
+  			a[i] == "Proteins";
+  			i++;
+  		}
+    }
+  }
+
+}
